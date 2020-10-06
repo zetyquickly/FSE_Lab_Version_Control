3d0806d45e17ac7ac81f6b5fe274d44cc03a9d5d
Nina-Konovalova <61041693+Nina-Konovalova@users.noreply.github.com>
2020-10-06 13:23:57 +0300

    Update storage.py
    Add the func add

--- storage.py
+++ storage.py
@@ -18,5 +18,9 @@ class Storage:
     def set(self):
         pass
     
-    def add(self):
-        pass
+    def add(self, key, value):
+        if key not in self.data:
+            return self.data.update({key: value})
+        else: 
+            raise KeyError
+           
