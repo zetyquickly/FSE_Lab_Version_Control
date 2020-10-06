6afbac71136e25373ce6c015e45443604ea97c45
affi <konovalova.np@phystech.edu>
2020-10-06 13:04:54 +0300

    add tets(add)

--- tests.py
+++ tests.py
@@ -1,7 +1,18 @@
 from storage import Storage
 
-def test_add():
-    pass
+def test_add ():
+     key, val = 'key', 'val'
+     wrong_key = 'key1'
+     storage = Storage({'key1':'val1'})
+     storage.add(key, val)
+     get_value = storage.get(key)
+     get_value == val
+     assert get_value == val, 'Wtong value for key was added'
+     try:
+         storage.add(wrong_key, val)
+         get_value = storage.get(key)
+     except KeyError:
+         print('Such key already exists, try another one')
 
 def test_remove():
     pass
@@ -19,10 +30,11 @@ def test_get():
     assert val is None, "Value for an unexisting key is not None"
 
 def run_tests():
+    test_get()
     test_add()
     test_remove()
     test_set()
-    test_get()
+    
 
 if __name__ == "__main__":
     run_tests()
